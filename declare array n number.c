67dac2cf9409c6240df34ce4345787a580996de0
ASHOK-WITHCODE <108255481+ASHOK-WITHCODE@users.noreply.github.com>
2022-06-28 22:24:35 +0530

    Add files via upload

---
+++ declare array n number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int n, i;
+	printf("enter the size of the array:\n");
+	scanf("%d",&n);
+	int A[n];
+	for (i=0; i<n; i++);
+	{
+	scanf ("%d",&A[i]);	
+	}
+	for (i=0;i<n;i++)
+	{
+		printf("%d",A[i]);
+	}
+	return 0;
+}
+
+
+
+
